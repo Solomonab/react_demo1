6b9201e95b39212517c7b762d4d314292062fb73
muliang <44419082+Solomonab@users.noreply.github.com>
2020-04-30 15:56:28 +0800

    no1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*
+应用根组件
+*/
+import React,{Component} from 'react'
+
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div>
+                app
+            </div>
+        )
+    }
+}
